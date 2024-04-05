2dfb8fc3c04d34e9cd272b4ecd5290e44b0df78e
Emes3Soft <161827303+emes3soft@users.noreply.github.com>
2024-04-05 17:45:13 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import find_packages, setup
+
+with open("Readme.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="c_bd",
+    version = "1.1",
+    description="Integração entre bases de dados, python, flask, html com a Bootstrap e DataTables",
+    package_dir={"":"c_bd"},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url = "https://github.com/emes3soft/c_bd",
+    author= "Emes3Soft",
+    author_email= "emes3soft@emes3soft.com",
+    license="GPLv3",
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License (GPLv3)",
+        "Programming Language :: Python :: 3.12",
+        "Operating System :: OS Independent",
+    ],
+    keywords="database basedados tabela table c_bd",
+    install_requires = [
+        "bcrypt >= 2.1.2",
+        "Flask >= 3.0.2",
+        "mysql-connector-python >= 8.3",
+        "Flask-Session >= 0.6.0",
+        "numpy >= 1.26.4"
+    ],
+    extras_require = {
+        "dev" : [
+            "pytest>=7.0",
+            "twine>=4.0.2"
+        ]
+    },
+    python_requires = ">=3.12",
+    package_data={'': ['*.zip']},
+    include_package_data=True,
+)
